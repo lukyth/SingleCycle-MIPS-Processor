891bd13e34fb2fe3c43f2d014aedd1ba7f23e048
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-05-13 15:49:57 +0700

    add instruction memory to registers process

--- processor.v
+++ processor.v
@@ -22,9 +22,7 @@ module processor(
 
   reg [31:0] pc = 32'h003FFFFC;
   wire [31:0] pc_plus4;
-
   adder pc_adder (.in(pc), .out(pc_plus4));
-
   always @(posedge clock) begin
     if (reset) begin
       pc <= 32'h003ffffc;
@@ -34,5 +32,32 @@ module processor(
     end
   end
 
+  wire [31:0] instruction;
+  inst_rom instruction_memory (
+    .clock(clock),
+    .reset(reset),
+    .addr_in(pc),
+    .data_out(instruction)
+  );
+
+  wire [4:0] write_register;
+  mux mux_write_register (
+    .in_0(instruction[20:16]),
+    .in_1(instruction[15:11]),
+    .select(RegDst),
+    .out(write_register)
+  );
+
+  wire [31:0] read_data_1, read_data_2, write_data;
+  register registers (
+    .write_register(write_register),
+    .write_data(write_data),
+    .read_register_1(instruction[25:21]),
+    .read_data_1(read_data_1),
+    .read_register_2(instruction[20:16]),
+    .read_data_2(read_data_2),
+    .reg_write(RegWrite),
+    .clock(clock)
+  );
 
 endmodule
